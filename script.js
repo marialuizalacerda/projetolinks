13ea1231cd917a9083de75c1f253454de3258d19
marialuizalacerda <lacerdamarialuiza1@gmail.com>
2023-05-26 16:05:41 -0300

    :tada initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+function toggleMode() {
+  const html = document.documentElement
+
+  //if (html.classList.contains("light")) {
+  //   html.classList.remove("light")
+  // } else {
+  //  html.classList.add("light")
+  // }
+  html.classList.toggle("light")
+
+  //pegar a tag img
+  const img = document.querySelector("#profile img")
+  // substituir a imagem
+  if (html.classList.contains("light")) {
+    // se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("alt", "Foto de Maria luiza usando óculos de sol")
+  } else {
+    // se tiver sem light mode, manter a imagem normal
+    img.setAttribute("src", "./assets/avatar-maria.png")
+    img.setAttribute("alt", "Foto de Maria luiza")
+  }
+}
+
+
+  
+  
